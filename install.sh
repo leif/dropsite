ffd97ef3b8d97354a65cb3bb251879944beb4bbb
Farrah Aine <dict@protonmail.com>
2018-02-21 12:57:04 +0000

    add dropsite to /usr/local/bin and oz-setup install it

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 set -x
 set -e
 
+cp -v dropsite.py /usr/local/bin/dropsite
+chmod +x /usr/local/bin/dropsite
 cp -v dropsite-roflcopter.json /etc/roflcoptor/filters/dropsite.json
 cp -v dropsite-oz.json /var/lib/oz/cells.d/dropsite.json
 cp -v dropsite-oz-whitelist.seccomp /var/lib/oz/cells.d/dropsite-whitelist.seccomp
 systemctl restart roflcoptor
 systemctl reload oz-daemon
+oz-setup install dropsite
