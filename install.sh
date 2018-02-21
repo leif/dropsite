73b4e8403044b8794933b533db3bf1f6f2920bf2
Farrah Aine <dict@protonmail.com>
2018-02-21 13:30:35 +0000

    update

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 set -x
 set -e
 
-cp -v dropsite.py /usr/local/bin/dropsite
-chmod +x /usr/local/bin/dropsite
+cp -v dropsite.py /usr/local/bin/dropsite-oz
+chmod +x /usr/local/bin/dropsite-oz
 cp -v dropsite-roflcopter.json /etc/roflcoptor/filters/dropsite.json
 cp -v dropsite-oz.json /var/lib/oz/cells.d/dropsite.json
 cp -v dropsite-oz-whitelist.seccomp /var/lib/oz/cells.d/dropsite-whitelist.seccomp
